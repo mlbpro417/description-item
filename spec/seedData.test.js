2160f1cbb0b027d12fa071c0037212794ee29b44
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-02-25 14:25:23 -0800

    made some change and added test

--- spec/seedData.test.js
+++ spec/seedData.test.js
@@ -1,29 +1,47 @@
-const seedDataTest = require('../data/seedDataGenerator');
+// const seedDataTest = require('../data/seedDataGenerator');
+const {
+  randoEl,
+  randoInt,
+  oneRandoInt,
+  randoDes,
+  mappingFunc,
+  mapp,
+} = require('../data/seedDataGenerator');
+
 
 describe('Seed Data Generator Function Testing', () => {
   test('randomElement should be a function', () => {
-    expect(typeof seedDataTest.randoEl).toBe('function');
+    expect(typeof randoEl).toBe('function');
   });
 
   test('getRandomInt should be a function', () => {
-    expect(typeof seedDataTest.randoInt).toBe('function');
+    expect(typeof randoInt).toBe('function');
+  });
+  test('getRandomInt should return a number', () => {
+    expect(typeof randoInt()).toBe('number');
   });
 
   test('getOneRandomInt should be a function', () => {
-    expect(typeof seedDataTest.oneRandoInt).toBe('function');
+    expect(typeof oneRandoInt).toBe('function');
+  });
+  test('getOneRandomInt should return a number', () => {
+    expect(typeof oneRandoInt()).toBe('number');
   });
 
   test('randomDescription should be a function', () => {
-    expect(typeof seedDataTest.randoDes).toBe('function');
+    expect(typeof randoDes).toBe('function');
+  });
+  test('randomDescription should return an array', () => {
+    expect(randoDes()).toBeInstanceOf(Array);
   });
 
   test('mapping should be a function', () => {
-    expect(typeof seedDataTest.mappingFunc).toBe('function');
+    expect(typeof mappingFunc).toBe('function');
   });
   test('mapping should return an array', () => {
-    expect(Array.isArray(seedDataTest.mapp)).toBe(true);
+    expect(mapp).toBeInstanceOf(Array);
   });
   test('mapping should return an array of 300 objects', () => {
-    expect(seedDataTest.mapp.length).toBe(300);
+    expect(mapp.length).toBe(300);
   });
 });
