0c2c012f1b378f87df89ce8b1ff1d66eb06f8ca9
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-02-24 18:40:21 -0800

    seed_data added and test passed

--- spec/seedData.test.js
+++ spec/seedData.test.js
@@ -1,25 +1,29 @@
 const seedDataTest = require('../data/seedDataGenerator');
 
 describe('Seed Data Generator Function Testing', () => {
-
   test('randomElement should be a function', () => {
-    expect(typeof seedDataTest.a).toBe('function');
+    expect(typeof seedDataTest.randoEl).toBe('function');
   });
 
-  test('randomElement should be a function', () => {
-    expect(typeof seedDataTest.b).toBe('function');
+  test('getRandomInt should be a function', () => {
+    expect(typeof seedDataTest.randoInt).toBe('function');
   });
 
-  test('randomElement should be a function', () => {
-    expect(typeof seedDataTest.c).toBe('function');
+  test('getOneRandomInt should be a function', () => {
+    expect(typeof seedDataTest.oneRandoInt).toBe('function');
   });
 
-  test('randomElement should be a function', () => {
-    expect(typeof seedDataTest.d).toBe('function');
+  test('randomDescription should be a function', () => {
+    expect(typeof seedDataTest.randoDes).toBe('function');
   });
 
-  test('randomElement should be a function', () => {
-    expect(typeof seedDataTest.e).toBe('function');
+  test('mapping should be a function', () => {
+    expect(typeof seedDataTest.mappingFunc).toBe('function');
   });
-
+  test('mapping should return an array', () => {
+    expect(Array.isArray(seedDataTest.mapp)).toBe(true);
+  });
+  test('mapping should return an array of 300 objects', () => {
+    expect(seedDataTest.mapp.length).toBe(300);
+  });
 });
