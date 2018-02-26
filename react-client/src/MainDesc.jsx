8418817a89ee9d6c564248ca328cd35366158110
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-02-25 19:07:17 -0800

    component added, working on proptypes

---
+++ react-client/src/MainDesc.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MainDesc = props => (
+  <div>
+    <div>
+      <p>{props.dataDesc.brand}</p>
+    </div>
+    <div>
+      <h1>{props.dataDesc.model}</h1>
+    </div>
+    <div>
+      <p>${props.dataDesc.price} & {props.dataDesc.shipping}</p>
+    </div>
+    <div>
+      <h3>{props.dataDesc.stock ? 'In Stock' : 'Temporarily Out of Stock'}</h3>
+    </div>
+  </div>
+);
+
+MainDesc.propTypes = {
+  data: PropTypes.shape({
+    brand: PropTypes.string.isRequired,
+    model: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    shipping: PropTypes.string.isRequired,
+    stock: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
+export default MainDesc;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import MainDesc from './MainDesc';
+import sampleData from '../sampleData';
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: sampleData,
+    };
+  }
   render() {
     return (
-      <div />
+      <div>
+        {this.state.data.map(x => <MainDesc dataDesc={x} />)}
+      </div>
     );
   }
 }
