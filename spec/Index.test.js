bcf114d1cfb688d294c6adf9ec885bf3767069bc
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-02-26 08:44:44 -0800

    fixed circleci

--- spec/Index.test.js
+++ spec/Index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Index from '../react-client/src/Index';
 
+
 describe('Index Component', () => {
   it('should be defined', () => {
     expect(Index).toBeDefined();
