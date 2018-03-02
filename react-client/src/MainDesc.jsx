892e2f606007648bb7e53eff1dbe47ef024462e6
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-03-01 18:21:36 -0800

    css and fixed seed/data

--- react-client/src/MainDesc.jsx
+++ react-client/src/MainDesc.jsx
@@ -13,7 +13,8 @@ const MainDesc = ({ dataDesc }) => (
     <div>
       <p id="price_shipping">Price: <span id="price"> ${dataDesc.price}</span>
         <span id="shipping"> {dataDesc.shipping === 'Prime' ?
-          <img src="../prime_logo.png" alt="Amazon Prime" /> : <span> & {dataDesc.shipping}.</span> }
+          <img src="../prime_logo.png" alt="Amazon Prime" /> :
+          <span> & {dataDesc.shipping}.</span>}
         </span>
       </p>
     </div>
