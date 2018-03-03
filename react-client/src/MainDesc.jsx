4eebf56b86de972018a8945f83fb81ba9f8f87a3
Nicolas Peyrichou <nicolas.peyrichou@live.fr>
2018-03-03 11:47:36 -0800

    fixed eslint

--- react-client/src/MainDesc.jsx
+++ react-client/src/MainDesc.jsx
@@ -19,7 +19,9 @@ const MainDesc = ({ dataDesc }) => (
       </p>
     </div>
     <div>
-      <h3 id="stock">{dataDesc.stock ? <span id="in">In Stock.</span> : <span id="out">Temporarily Out of Stock.</span>}</h3>
+      <h3 id="stock">{dataDesc.stock ? <span id="in">In Stock.</span> :
+      <span id="out">Temporarily Out of Stock.</span>}
+      </h3>
     </div>
     <div id="list_desc">
       {dataDesc.description.map(x => <ListDesc item={x} key={x} />)}
